822190c28fbce83b478b8134b915d1f01dfb54b3
Harshal623 <89270049+Harshal623@users.noreply.github.com>
2021-10-30 10:28:27 -0400

    Add files via upload

---
+++ C82main.js
@@ -0,0 +1,37 @@
+canvas = document.getElementById('mycanvas')
+ctx = canvas.getContext('2d')
+var mouse_event = " "
+var last_x, last_y
+var color, line_width
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e){
+    mouse_event = "mouse_down"
+    color = document.getElementById("colorname").value 
+    line_width = document.getElementById("line_width").value
+}
+canvas.addEventListener("mouseup", my_mouseup);
+function my_mouseup(e){
+    mouse_event = "mouse_up"
+}
+canvas.addEventListener("mouseleave", my_mouseleave);
+function my_mouseleave(e){
+    mouse_event = "mouse_leave"
+}
+canvas.addEventListener("mousemove", my_mousemove)
+function my_mousemove(e){
+    current_x = e.clientX - canvas.offsetLeft
+    current_y = e.clientY - canvas.offsetTop
+    if(mouse_event == "mouse_down"){
+        ctx.beginPath()
+        ctx.strokeStyle = color
+        ctx.lineWidth = line_width
+        ctx.moveTo(last_x, last_y)
+        ctx.lineTo(current_x, current_y)
+        ctx.stroke()
+    }
+    last_x = current_x
+    last_y = current_y
+}
+function cleararea(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
